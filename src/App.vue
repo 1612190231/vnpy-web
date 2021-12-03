5636d86ceb6a0274444674976d6248ab0f118540
鲁铖锴 <1390813832@qq.com>
2021-12-03 21:20:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1 @click="changeMsg">{{msg}}</h1>
+  <input type="text" v-model="msg" />
+  <div v-bind:id = "id"></div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data(){
+	  return{
+		  msg:"helloworld",
+		  id: "12" 
+	  }
+  },
+  methods:{
+	  changeMsg(){
+		  this.msg="luck帅呆了！"
+		  this.id = "d2"
+	  }
+  }
+}
+</script>
+
+<style>
+	#d1{
+		width:100px;
+		
+	},
+	#d2{
+		width: 50px;
+	}
+</style>
